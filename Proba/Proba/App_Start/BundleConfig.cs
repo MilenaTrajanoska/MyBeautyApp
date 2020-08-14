7ccca53b3551a6f90906f5672c90d6184e909a8e
Anastasija99 <anastasija.petrovska.5@gmail.com>
2020-08-14 15:22:50 +0200

    Register Client i Salon css promeni

--- Proba/Proba/App_Start/BundleConfig.cs
+++ Proba/Proba/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace Proba
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/css-stilovi/StyleSheet1.css"
+                      "~/Content/css-stilovi/StyleSheet1.css",
+                      "~/Content/css-stilovi/StiloviZaFormi.css"
                       ));
         }
     }
