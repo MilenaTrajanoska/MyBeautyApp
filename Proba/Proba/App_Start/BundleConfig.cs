f8316e47eaf8de16ec2a091f35624d43ed12afb2
MilenaTrajanoska <68651081+MilenaTrajanoska@users.noreply.github.com>
2020-09-05 18:33:05 +0200

    nesho rezervacii

--- Proba/Proba/App_Start/BundleConfig.cs
+++ Proba/Proba/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace Proba
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootbox.js"));
+                      "~/Scripts/bootbox.js",
+                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
