3949315e8da46dd0dfcdff05c5772494555a4972
MilenaTrajanoska <68651081+MilenaTrajanoska@users.noreply.github.com>
2020-09-12 00:56:46 +0200

    cleaned database and fixed gallery

--- Proba/Proba/Scripts/gallery.js
+++ Proba/Proba/Scripts/gallery.js
@@ -14,16 +14,9 @@ function plusSlides(n) {
         showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-        showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++){
